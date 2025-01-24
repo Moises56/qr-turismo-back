9bf208438ba0fe4cd8ef120d4b92dd2234117063
Moises56 <moiavil56@gmail.com>
2025-01-24 12:50:17 -0600

    .htaccess

--- src/api/suscribe/suscribe.service.ts
+++ src/api/suscribe/suscribe.service.ts
@@ -10,13 +10,11 @@ export class SuscribeService {
   async create(createSuscribeDto: CreateSuscribeDto) {
     // Convert eventDate to a valid ISO-8601 format if it's provided
     const formattedEventDate = new Date(createSuscribeDto.eventDate);
-  
     // Add the formatted date back into the DTO
     const data = {
       ...createSuscribeDto,
       eventDate: formattedEventDate,
     };
-  
     // Insert the data into the database
     return this.prisma.suscripcion.create({
       data,
